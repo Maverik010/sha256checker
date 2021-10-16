8f2a3867a943e451f1adc271dee14ac6359009f0
sztukant <sztukant@gmail.com>
2021-10-16 15:21:06 +0200

    inne podejscie do tematu

--- sprawdzSume.java
+++ sprawdzSume.java
@@ -1,60 +1,57 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
 
 
 public class sprawdzSume {
-    
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
     public static void main(String[] args) {
 
         if (args.length != 2) {
-            System.out.println(consoleColors.YELLOW+"Użycie: sprawdzSume <ścieżka_do_pliku> <ścieżka_do_sumy_shaDigest>"+consoleColors.RESET);
-        }
-        else try {
+            System.out.println(consoleColors.YELLOW + "Użycie: sprawdzSume <ścieżka_do_pliku> <ścieżka_do_sumy_shaDigest>" + consoleColors.RESET);
+        } else try {
 
-            MessageDigest shaDigest = MessageDigest.getInstance("SHA-256"); // zainicjuj algorytm SHA-256
-            FileInputStream sourceFile = new FileInputStream(args[0]);      // ścieżka do pliku źródłowego
-            FileInputStream hashFile = new FileInputStream(args[1]);      // ścieżka do pliku .sha256
-            //  System.out.print(args[0]);
-            //  System.out.print(" "+args[1]);
-            
-        
-        byte[] data = new byte[1024]; // przydzielenie długości sha-256 zakresu pamięci
-        int read ; // iterator
-
-        while ((read = sourceFile.read(data)) != -1) {
-            shaDigest.update(data, 0, read);
-        }
-        sourceFile.close();
-
-        byte[] hashBytes = shaDigest.digest();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < hashBytes.length; i++) {
-          sb.append(Integer.toString((hashBytes[i] & 0xff) + 0x100, 16).substring(1));
-        }
-        hashFile.close();
-        String fileHash = sb.toString();
-    String hash = ""; //string do hashFileContent
-        sb.delete(0, sb.length());
-        StringBuilder hashFilecontent = new StringBuilder();
-
-        while(hashFile.available() != 0){
-            hashFilecontent.append(hashFile);
-            hash = hashFilecontent.toString();
-        }
-            if(hash.equals(fileHash)){
-                System.out.println(consoleColors.YELLOW+hashFilecontent);
-                System.out.println(consoleColors.YELLOW+shaDigest);
-                System.out.println(consoleColors.GREEN+"Sygnatura (shaDigest) jest OK"+consoleColors.RESET);
+            if (args.length != 2) {
+                System.out.println("Użycie: sprawdzSume <ścieżka_do_pliku> <ścieżka_do_sumy_shaDigest>");
             }else{
-                System.out.println(consoleColors.YELLOW+hashFilecontent);
-                System.out.println(consoleColors.YELLOW+shaDigest);
-                System.out.println(consoleColors.RED+"Sygnatura (shaDigest) pliku się nie zgadza z zadeklarowaną!"+consoleColors.RESET);
+                FileInputStream sourceFile = new FileInputStream(args[0]);      // ścieżka do pliku źródłowego
+                FileInputStream hashFile = new FileInputStream(args[1]);      // ścieżka do pliku .sha256
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                String originalString = sourceFile.toString();
+                byte[] encodedhash = digest.digest(
+                        originalString.getBytes(StandardCharsets.UTF_8));
+                String encoded = bytesToHex(encodedhash);                    //String z hexem hasha
+                String hashFromFile = "";                  //Hash z pliku
+
+                BufferedReader br =
+                        new BufferedReader( new InputStreamReader(hashFile, StandardCharsets.UTF_8 ));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while(( line = br.readLine()) != null ) {
+                    sb.append( line );
+                    hashFromFile = sb.toString();
+                }
+                System.out.println(encoded + "   " + encoded.length());
+                System.out.println(hashFromFile + "   " + encoded.length());
+                if (encoded.equals(hashFromFile)) {
+                    System.out.println("Hash prawidlowy");
+                } else {
+                    System.out.println("Hash zjebany");
+                }
             }
-        
-
-
-        } catch (Exception e) {
+        }catch (Exception e) {
             System.err.println("Wyjątek: " + e);
         }
-}
+    }
 }
